6102942f351636d94405828df93eb1266b48c2df
Amar Lakshya <amar.lakshya@protonmail.com>
2025-09-16 13:36:50 +0100

    fix: actually correct the hash for weechat-vimode plugin

--- pkgs/applications/networking/irc/weechat/scripts/vimode/default.nix
+++ pkgs/applications/networking/irc/weechat/scripts/vimode/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
 
   src = fetchurl {
     url = "https://github.com/GermainZ/weechat-vimode/blob/192eb3ef0fa2166e1d35a163434b9e446226a4fa/vimode.py";
-    sha256 = "9596bc188d4a32569d05d597d38dc80d800913f64f6c4e86e4a970fd1c332d9e";
+    sha256 = "1zap011l3wlxd61j9xyqamzb85srvvwy7qg5sjsrp8sa77snl0r8";
   };
 
   dontUnpack = true;
